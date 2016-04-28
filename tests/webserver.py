f29d087aea243b765d8c42cdd2e6c31573d9c2d5
rahulraghu94 <rahulraghu94@gmail.com>
2016-04-28 17:09:09 +0530

    Removed copy of a testfile, webserver.py in main directory. Cleaned up tests/webserver.py

--- tests/webserver.py
+++ tests/webserver.py
@@ -10,26 +10,28 @@ class webServerHandler(BaseHTTPRequestHandler):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
+                
                 output = ""
                 output += "<html><body>"
                 output += "<h1>Hello!</h1>"
                 output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
                 output += "</body></html>"
+                
                 self.wfile.write(output.encode(encoding = 'utf_8'))
-                print (output)
                 return
 
             if self.path.endswith("/hola"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
+                
                 output = ""
                 output += "<html><body>"
                 output += "<h1>&#161 Hola !</h1>"
                 output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
                 output += "</body></html>"
+                
                 self.wfile.write(output)
-                print (output.encode(encoding = 'utf_8'))
                 return
 
         except IOError:
@@ -40,15 +42,15 @@ class webServerHandler(BaseHTTPRequestHandler):
             self.send_response(301)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
+            
             ctype, pdict = cgi.parse_header(
                 self.headers.get('content-type'))
+            
             pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
-            print("Parse done!", pdict)
             if ctype == 'multipart/form-data':
-                print("yes, ctype is", ctype)
                 fields = cgi.parse_multipart(self.rfile, pdict)
-                print("Fields:", fields) 
                 messagecontent = fields.get('message')
+            
             output = b""
             output += b"<html><body>"
             output += b" <h2> Okay, how about this: </h2>"
@@ -56,9 +58,9 @@ class webServerHandler(BaseHTTPRequestHandler):
             output += messagecontent[0]
             output += b'''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
             output += b"</body></html>"
-            print(output)
+            
             self.wfile.write(output)
-            print (output)
+            
         except Exception as e:
             print (e)
 
