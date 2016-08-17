38786a8387bf0346409b2c9d93cc287fde261210
rahulraghu94 <rahulraghu94@gmail.com>
2016-08-17 20:38:55 +0530

    Added functionality to allow a user to login using google Oauth

--- server.py
+++ server.py
@@ -4,6 +4,15 @@ from flask_httpauth import HTTPBasicAuth
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from database_setup import Catalog, Base, Wine
+import random, string
+from flask import session as login_session
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response 
+import requests
+from oauth2client.client import AccessTokenCredentials
 
 engine = create_engine('sqlite:///wineCatalog.db')
 Base.metadata.bind = engine
@@ -11,6 +20,102 @@ DBSession = sessionmaker(bind=engine)
 session = scoped_session(DBSession)
 
 app = Flask(__name__)
+
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+
+@app.route('/gconnect', methods = ['POST'])
+def gconnect():
+	#print("inside gconnect...")
+	#print(request.args.get('state'))
+
+	if request.args.get('state') != login_session['state']:
+		response = make_response(json.dumps('Invalid state parameter you mofofs'), 401)
+		response.headers['Content-tyoe'] = 'application/json'
+		return response
+
+	code = request.data
+
+	try:
+		oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+		oauth_flow.redirect_uri = 'postmessage'
+		credentials = oauth_flow.step2_exchange(code)
+	except FlowExchangeError:
+		response = make_response(json.dumps('Auth Code Upgradation Failure'), 401)
+		response.headers['Content-tyoe'] = 'application/json'
+		return response
+
+	access_token = credentials.access_token
+	url = 'https://www.googleapis.com/oauth2/v2/tokeninfo?access_token='
+	url = url + access_token
+	h = httplib2.Http()
+	#print(url)
+	result = json.loads(h.request(url, 'GET')[1].decode('utf-8'))
+
+	if result.get('error') is not None:
+		response - make_response(json.dumps(result.get('error')), 500)
+		response.headers['Content-tyoe'] = 'application/json'
+
+	gplus_id = credentials.id_token['sub']
+	#print(gplus_id)
+
+	if result['user_id'] != gplus_id:
+		response = make_response(json.dumps("Token ID mismatch"), 401)
+		response.headers['Content-tyoe'] = 'application/json'
+		return response
+
+	if result['issued_to'] != CLIENT_ID:
+		response = make_response(json.dumps('Client ID mismatch'), 401)
+		print("Check client ID")
+		response.headers['Content-tyoe'] = 'application/json'
+		return response
+
+	stored_credentials = login_session.get('credentials')
+	stored_gplus_id = login_session.get('gplus_id')
+
+	if stored_credentials is not None and gplus_id == stored_gplus_id:
+		response = make_response(json.dumps('User is already logged in'), 200)
+		response.headers['Content-tyoe'] = 'application/json'
+
+	login_session['credentials'] = credentials.access_token
+	credentials = AccessTokenCredentials(login_session['credentials'], 'user-agent-value')
+	login_session['gplus_id'] = gplus_id
+
+	userinfo_url = ("https://www.googleapis.com/oauth2/v2/userinfo")
+	params = {'access_token' : credentials.access_token, 'alt' : 'json'}
+
+	#print("Sending the request to...: " , userinfo_url)
+
+	answer = requests.get(userinfo_url, params=params)
+	data = json.loads(answer.text)
+
+	#print("Data is..." , data)
+
+	login_session['username'] = data["name"]
+	login_session['picture'] = data["picture"]
+	login_session['email'] = data["link"]
+
+	#print(login_session['username'])
+	#print(login_session['picture'])
+
+	output = ''
+	output += '<h1> Welcooome'
+	output += login_session['username']
+	output += '!</h1>'
+
+	output += '<img src = '
+	output += login_session['picture']
+	output += ' "style = "width:300px; height:300px;">'
+
+	print(output)
+
+	return render_template('after_login.html', NAME=login_session['username'], PIC=login_session['picture'])
+
+@app.route("/login")
+def login():
+	state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+
+	login_session['state'] = state
+	return render_template('login.html', STATE=state)
 
 ###############################################################################
 # JSON Route
@@ -42,7 +147,7 @@ def main_page():
 	this_session = session()
 
 	catalog = this_session.query(Catalog)
-	print("hello Word")
+	print("hello Woord")
 	session.remove()
 	return render_template('index.html')
 
